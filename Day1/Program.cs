3b7c583a1fb8c8ede85237e67cb8b479dcb7d25b
Knuddy <deanknuddy@gmail.com>
2025-05-11 20:53:44 +1200

    day 1 complete

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Day 1");
+﻿var input = File.ReadAllText("input.txt");
+
+Console.WriteLine($"Input Length: {input.Length}");
+
+/* Part One */
+
+Console.WriteLine($"Final floor: {input.ToList().Sum(v => v.Equals('(') ? 1 : -1)}");
+
+/* Part Two */
+
+var floor = 0;
+var position = 0;
+
+while (floor >= 0)
+{
+    floor += input[position].Equals('(') ? 1 : -1;
+    position++;
+}
+
+Console.WriteLine($"Position at which the basement was first entered: {position}");
