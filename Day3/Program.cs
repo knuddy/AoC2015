e5b7e6411dd92ed92c626205e8d4c2891bbb23f8
Knuddy <deanknuddy@gmail.com>
2025-05-12 18:28:55 +1200

    Day3 complete

---
+++ Day3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+
+var input = File.ReadAllText("input.txt");
+
+void UpdatePosition(char arrow, ref Point position)
+{
+    switch (arrow)
+    {
+        case '^':
+            position.Y--;
+            break;
+        case 'v':
+            position.Y++;
+            break;
+        case '<':
+            position.X--;
+            break;
+        case '>':
+            position.X++;
+            break;
+    }
+}
+
+/* Part One */
+{
+    var position = new Point();
+    var houses = new HashSet<Point>();
+    
+    foreach (char arrow in input)
+    {
+        UpdatePosition(arrow, ref position);
+        houses.Add(position);
+    }
+
+    Console.WriteLine($"P1 Unique houses visited {houses.Count} ");
+}
+
+
+/* Part Two */
+var santaPosition = new Point();
+var roboPosition = new Point();
+var houses2 = new HashSet<Point>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        UpdatePosition(input[i], ref santaPosition);
+        houses2.Add(santaPosition);
+    }
+    else
+    {
+        UpdatePosition(input[i], ref roboPosition);
+        houses2.Add(roboPosition);
+    }
+}
+
+Console.WriteLine($"P2 Unique houses visited {houses2.Count} ");
+
