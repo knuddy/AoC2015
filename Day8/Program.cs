f8067947d69a9b06136f034751f4a006f1f00b27
Knuddy <deanknuddy@gmail.com>
2025-05-15 20:00:52 +1200

    Day 8 complete - part two from reddit

---
+++ Day8/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+/* Part One */
+
+var p1Solution = input.Select(s => s.Length - (Regex.Unescape(s).Length - 2)).Sum();
+Console.WriteLine($"P1 Number of characters of code minus the number of characters in memory for the input strings is: {p1Solution}");
+
+var p2Solution = input.Sum(w => w.Replace("\\", "AA").Replace("\"", "BB").Length + 2 - w.Length);
+Console.WriteLine($"P2 number of characters in represent the newly encoded strings minus the number of characters of code in each original: {p2Solution}");
